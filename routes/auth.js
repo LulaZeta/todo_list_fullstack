0c77c8b765bb7200d4c585a3dc232be6997323cd
Luciana Zanetti <luciana.zeta@gmail.com>
2022-06-01 14:07:57 -0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
+// @route GET /api/auth/test
+// @desc  Test the auth route
+// @access Public
 router.get('/test', (req, res) => {
   res.send('auth routh working');
+});
+
+// @route POST /api/auth/register
+// @desc  Create a new user
+// @access Public
+router.get('/register', async (req, res) => {
+  try {
+    //  create a new user
+
+    const newUser = new User({
+      email: req.body.email,
+      //password: req.body.password,
+      name: req.body.name,
+    });
+
+    //save the User to de database
+    const savedUser = await newUser.save();
+
+    //return the new user
+    return res.json(savedUser);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
 });
 
 module.exports = router;
