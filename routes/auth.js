03e70e5a36221846a8ef342464953e08a876f3c3
Luciana Zanetti <luciana.zeta@gmail.com>
2022-06-02 10:18:20 -0300

    Update auth.js
    update validation

--- routes/auth.js
+++ routes/auth.js
@@ -44,11 +44,46 @@ router.post('/register', async (req, res) => {
     //save the User to de database
     const savedUser = await newUser.save();
 
+    // no retornamos la password..tenemos q crear un nuevo objeto y luego le quitamos la pass
+    const userToReturn = { ...savedUser._doc };
+    delete userToReturn.password;
+
     //return the new user
-    return res.json(savedUser);
+    return res.json(userToReturn);
   } catch (err) {
     console.log(err);
     res.status(500).send(err.message);
+  }
+});
+
+// @route POST /api/auth/loggin
+// @desc  Login user and return a access token
+// @access Public
+router.post('/login', async (req, res) => {
+  try {
+    // check for the user
+    const user = await User.findOne({
+      email: new RegExp('^' + req.body.email + '$', 'i'),
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ error: 'There was a ploblem with your login credentials' });
+    }
+    const passwordMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordMatch) {
+      return res
+        .status(400)
+        .json({ error: 'There was a ploblem with your login credentials' });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send(err.message);
   }
 });
 
