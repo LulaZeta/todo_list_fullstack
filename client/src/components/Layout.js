6ed6d613752569271c21ad5a5a1f8ba59b8c1ffd
Luciana Zanetti <luciana.zeta@gmail.com>
2022-06-03 15:55:48 -0300

    logout/loading

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AuthBox from './AuthBox';
 import Header from './Header';
+import { useGlobalContext } from '../context/GlobalContext';
 
 const Layout = () => {
-  return (
+  const { fetchingUser } = useGlobalContext();
+
+  return fetchingUser ? (
+    <div className="loading">
+      <h1>Loading</h1>
+    </div>
+  ) : (
     <BrowserRouter>
       <Header />
       <Routes>
