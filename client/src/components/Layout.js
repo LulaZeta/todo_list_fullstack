4a4385ab5f858bbf481268a1a94032f4e7344ef0
Luciana Zanetti <luciana.zeta@gmail.com>
2022-06-03 18:25:19 -0300

    DashBoard

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AuthBox from './AuthBox';
 import Header from './Header';
+import Dashboard from './Dashboard';
 import { useGlobalContext } from '../context/GlobalContext';
 
 const Layout = () => {
@@ -17,6 +18,7 @@ const Layout = () => {
       <Routes>
         <Route exact path="/" element={<AuthBox />} />
         <Route path="/register" element={<AuthBox register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
