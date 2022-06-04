68815d2c78d1d30bad47b6bc6415a4df58a09c2d
Luciana Zanetti <luciana.zeta@gmail.com>
2022-06-04 06:47:09 -0300

    dashboard update

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,7 +3,7 @@ import { useGlobalContext } from '../context/GlobalContext';
 import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
-  const { user } = useGlobalContext();
+  const { user, completeToDos, incompleteToDos } = useGlobalContext();
   const navigate = useNavigate();
 
   React.useEffect(() => {
@@ -11,7 +11,23 @@ const Dashboard = () => {
       navigate('/');
     }
   }, [user, navigate]);
-  return <div>Dashboard</div>;
+  return (
+    <div className="dashboard">
+      <div className="todos">
+        {incompleteToDos.map((todo) => (
+          <h1 key={todo._id}>{todo.content}</h1>
+        ))}
+      </div>
+      {completeToDos.length > 0 && (
+        <div className="todos">
+          <h2 className="todos__title">Complete ToDo's</h2>
+          {completeToDos.map((todo) => (
+            <h1 key={todo._id}>{todo.content}</h1>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
