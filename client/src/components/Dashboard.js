b947cc7d8f3ff6bc6e68b6aedc9e156895bcef04
Luciana Zanetti <luciana.zeta@gmail.com>
2022-06-04 07:54:33 -0300

    ToDo

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useGlobalContext } from '../context/GlobalContext';
 import { useNavigate } from 'react-router-dom';
+import ToDoCard from './ToDoCard';
 
 const Dashboard = () => {
   const { user, completeToDos, incompleteToDos } = useGlobalContext();
@@ -14,15 +15,15 @@ const Dashboard = () => {
   return (
     <div className="dashboard">
       <div className="todos">
-        {incompleteToDos.map((todo) => (
-          <h1 key={todo._id}>{todo.content}</h1>
+        {incompleteToDos.map((toDo) => (
+          <ToDoCard toDo={toDo} key={toDo._id} />
         ))}
       </div>
       {completeToDos.length > 0 && (
         <div className="todos">
           <h2 className="todos__title">Complete ToDo's</h2>
-          {completeToDos.map((todo) => (
-            <h1 key={todo._id}>{todo.content}</h1>
+          {completeToDos.map((toDo) => (
+            <ToDoCard toDo={toDo} key={toDo._id} />
           ))}
         </div>
       )}
